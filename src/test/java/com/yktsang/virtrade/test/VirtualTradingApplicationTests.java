9d2282d0346af51fbd89769808afd0c181a73f64
yktsang01 <68239425+yktsang01@users.noreply.github.com>
2024-05-18 15:11:09 +0800

    Initial commit

---
+++ src/test/java/com/yktsang/virtrade/test/VirtualTradingApplicationTests.java
@@ -0,0 +1,30 @@
+/*
+ * VirtualTradingApplicationTests.java
+ *
+ * Virtual Trading is a web application simulating online stock trading.
+ *
+ * This class or interface is part of the Virtual Trading project.
+ * The class or interface must not be used outside of this context.
+ */
+package com.yktsang.virtrade.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Provides the test cases for the application.
+ *
+ * @author Tsang Yiu Kee Kay
+ * @version 1.0
+ */
+@SpringBootTest
+public class VirtualTradingApplicationTests {
+
+    /**
+     * Tests loading the context.
+     */
+    @Test
+    public void contextLoads() {
+    }
+
+}
